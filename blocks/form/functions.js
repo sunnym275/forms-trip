ed5e936f0d1892f0b4226e1d417839f30343d231
Vijay Kumar Jalagari <jalagari@adobe.com>
2024-09-10 16:25:53 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -1,15 +1,4 @@
 /**
- * Get Full Name
- * @name getFullName Concats first name and last name
- * @param {string} firstname in Stringformat
- * @param {string} lastname in Stringformat
- * @return {string}
- */
-function getFullName(firstname, lastname) {
-  return `${firstname} ${lastname}`.trim();
-}
-
-/**
  * Submit form in excel
  * @name submitToSpreadSheet Submit data to Spreadsheet
  * @param {string} url in spreadsheet
@@ -40,24 +29,5 @@ function submitToSpreadSheet(url, ...args) {
   }
 }
 
-/**
- * Calculate the number of days between two dates.
- * @param {*} endDate
- * @param {*} startDate
- * @returns {number} returns the number of days between two dates
- */
-function days(endDate, startDate) {
-  const start = typeof startDate === 'string' ? new Date(startDate) : startDate;
-  const end = typeof endDate === 'string' ? new Date(endDate) : endDate;
-
-  // return zero if dates are valid
-  if (Number.isNaN(start.getTime()) || Number.isNaN(end.getTime())) {
-    return 0;
-  }
-
-  const diffInMs = Math.abs(end.getTime() - start.getTime());
-  return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-}
-
 // eslint-disable-next-line import/prefer-default-export
-export { getFullName, days, submitToSpreadSheet };
+export { submitToSpreadSheet };
