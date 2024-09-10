4375809735dc5bcaf719bad2dd30159a503ce25a
Vijay Kumar Jalagari <jalagari@adobe.com>
2024-09-10 11:13:00 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -10,6 +10,37 @@ function getFullName(firstname, lastname) {
 }
 
 /**
+ * Submit form in excel
+ * @name submitToSpreadSheet Submit data to Spreadsheet
+ * @param {string} url in spreadsheet
+ */
+function submitToSpreadSheet(url, ...args) {
+  const form = document.querySelector('form');
+  const valid = form.checkValidity();
+  if (valid) {
+    const [globals] = args;
+    const data = globals.functions.exportData();
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ data }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          submitFailure({}, form);
+        } else {
+          submitSuccess({}, form);
+        }
+      })
+      .catch(() => {
+        submitFailure({}, form);
+      });
+  }
+}
+
+/**
  * Calculate the number of days between two dates.
  * @param {*} endDate
  * @param {*} startDate
